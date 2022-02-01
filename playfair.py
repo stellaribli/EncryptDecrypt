80cad90b9112b2b71bffee554f50eef13f1b3448
Stella Ribli <18219027@std.stei.itb.ac.id>
2022-02-02 00:27:13 +0700

    Update playfair.py

--- playfair.py
+++ playfair.py
@@ -1,9 +1,5 @@
 from numpy import array
 
-
-print(ord("A")) #65
-print(ord("Z")) #90
-
 arrayAlphabet = [0 for i in range(26)]
 arrayBujurSangkar = [["" for i in range(5)] for j in range(5)]
 teksKey = input("Masukkan Key: ")
@@ -13,6 +9,7 @@ print(teksKey)
 keyBaru = ""
 bujurX = 0
 bujurY = 0
+#Create Bujur Sangkar
 for huruf in teksKey:
     index = ord(huruf)-65
     if arrayAlphabet[index] == 0:
@@ -24,8 +21,6 @@ for huruf in teksKey:
         else:
             bujurY+=1
 i=0
-
-
 if not (bujurY==4 and bujurX==4):
     for isiKosong in arrayAlphabet: 
         if isiKosong == 0 and i!=9:
@@ -37,21 +32,23 @@ if not (bujurY==4 and bujurX==4):
                 bujurY+=1
         i+=1
 
-print(arrayBujurSangkar)
-# if bujurX!=4 and bujurY !=4:
-#     for isiKosong in arrayAlphabet: 
-#         i = 0
-#         if isiKosong == 0:
-#             arrayBujurSangkar[bujurX][bujurY] = 1
-#             if bujurY==4:
-#                 bujurX+=1
-#                 bujurY=0
-#             else:
-#                 bujurY+=1
-#         i = i+1  
-# print(arrayAlphabet)
-# print(arrayBujurSangkar)
-# def playfairKey(teks):
-#     for i in teks:
-        
+teksPesan = input("Masukkan Pesan: ")
+teksPesan = teksPesan.replace("J","")
 
+arrayTeks = []
+
+currentNumber = 0 
+
+ind = 0
+while ind<(len(teksPesan)-1):
+    if teksPesan[ind] == teksPesan[ind+1]:
+        arrayTeks.append([teksPesan[ind],'X'])
+        ind+=1
+    else:
+        arrayTeks.append([teksPesan[ind],teksPesan[ind+1]])
+        ind+=2
+if ind<len(teksPesan):
+    arrayTeks.append([teksPesan[ind],'X'])
+    
+print(ind)
+print(arrayTeks)
